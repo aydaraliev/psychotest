50069be3db5048158c01552f197173a89fd4419d
Rustam <archer1strong@gmail.com>
2018-06-09 04:22:29 +0600

    add sending object

--- psycho_front/src/app/components/form-feedback/form-feedback.component.ts
+++ psycho_front/src/app/components/form-feedback/form-feedback.component.ts
@@ -9,14 +9,24 @@ import { Router } from '@angular/router';
 })
 
 export class FormFeedbackComponent implements DoCheck {
-  lang = 0;
-  title = ['Помогите нам улучшить наши исследования', 'Келгиле, биздин изилдөө жакшыртууга жардам берет'];
-  /* tslint:disable */
-  subtitle = ['Все ответы являются необязательными. Если вы предпочитаете не отвечать на какой-либо конкретный вопрос, это прекрасно, если оставить его пустым. Любые данные, которые вы согласны предоставить, будут полезны.', 'Бардык жооптор бар. Сиз, балким, бир суроого жооп эмес, керек болсо, ал аны бош калтырып үчүн жакшы. Сиз камсыз кылууга макул болбосун маалыматтар жардам берет.'];
-  /* tslint:enable */
-  dob = ['Дата рождения', 'Туулган датасы'];
-  country = ['Я родился в', 'Мен төрөлгөм'];
-  gender = ['Пол', 'Калаасы'];
+  // lang = 0;
+  // title = ['Помогите нам улучшить наши исследования', 'Келгиле, биздин изилдөө жакшыртууга жардам берет'];
+  // /* tslint:disable */
+  // subtitle = ['Все ответы являются необязательными. Если вы предпочитаете не отвечать на какой-либо конкретный вопрос, это прекрасно, если оставить его пустым. Любые данные, которые вы согласны предоставить, будут полезны.', 'Бардык жооптор бар. Сиз, балким, бир суроого жооп эмес, керек болсо, ал аны бош калтырып үчүн жакшы. Сиз камсыз кылууга макул болбосун маалыматтар жардам берет.'];
+  // /* tslint:enable */
+  // dob = ['Дата рождения', 'Туулган датасы'];
+  // country = ['Я родился в', 'Мен төрөлгөм'];
+  // gender = ['Пол', 'Калаасы'];
+  dob = '';
+  gender = '';
+  country = '';
+  nationality = '';
+  education = '';
+  family = '';
+  work = '';
+  city = '';
+  email = '';
+  sendEmail = '';
 
   constructor(
     // private langService: LanguageService,
@@ -24,10 +34,23 @@ export class FormFeedbackComponent implements DoCheck {
   ) { }
 
   ngDoCheck() {
-    this.lang = Number(localStorage.lang);
+    // this.lang = Number(localStorage.lang);
   }
 
   onSubmit() {
+    const sendingObject = {
+      dob: this.dob,
+      gender: this.gender,
+      country: this.country,
+      nationality: this.nationality,
+      education: this.education,
+      family: this.family,
+      work: this.work,
+      city: this.city,
+      email: this.email,
+      sendEmail: this.sendEmail
+    };
+    console.log(sendingObject);
     this.router.navigate(['/results']);
   }
 }
