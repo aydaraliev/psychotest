e1fde3f5543457b44034a890c3d081c33df8878f
Rustam <archer1strong@gmail.com>
2018-06-18 11:02:10 +0600

    add kyrgyz letters

--- psycho_front/src/app/components/voted/voted.component.ts
+++ psycho_front/src/app/components/voted/voted.component.ts
@@ -34,6 +34,10 @@ export class VotedComponent {
       );
   };
 
+  addLetter (letter: string, field: string) :void {
+    this[field] = this[field] + letter;
+  }
+
   tagVoter (voter: Object) :void {
     localStorage.setItem('voter', JSON.stringify(voter));
     this.router.navigate(['/feedback']);
