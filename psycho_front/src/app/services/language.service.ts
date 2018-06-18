e9caca0ce517b04d6b418acf5b8e40d3fdac7f76
Rustam <archer1strong@gmail.com>
2018-06-18 21:05:07 +0600

    uuid

--- psycho_front/src/app/services/language.service.ts
+++ psycho_front/src/app/services/language.service.ts
@@ -41,11 +41,14 @@ export class LanguageService {
 
   sendUser (object: any): Observable<Response> {
     const url = `${this.apiUrl}/tests/results`;
+    const uuid4 = JSON.parse(localStorage.getItem('uuid4'));
+
     const sendingObject = {
       user: object,
       results: JSON.parse(localStorage.getItem('results')),
       voter: JSON.parse(localStorage.getItem('voter')),
       responses: JSON.parse(localStorage.getItem('backQuestions')),
+      uuid4
     };
     // localStorage.removeItem('results');
     localStorage.removeItem('voter');
